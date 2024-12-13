f5624bc5a499d9118d7b4b6d3c4aa58328449fe8
Anas Muhammed <anasmhdoffical006@gmail.com>
2024-12-13 11:24:52 +0530

    CourseCloud backend v.0.0.1

--- controller/progress_controller.js
+++ controller/progress_controller.js
@@ -5,13 +5,13 @@ import video_tutorial_progress_model from "../models/video_progress.js"
 const update_video_progress = async (req, res) => {
     try {
         const { student_id, course_id, lesson_id, duration, recent_whatched_time } = req.body
-        const get_video_progress = await video_tutorial_progress_model.findOne({ lesson_id })
+        const get_video_progress = await video_tutorial_progress_model.findOne({ student_id, lesson_id })
         if (get_video_progress) {
             get_video_progress.recently_watched_time = recent_whatched_time
             get_video_progress.tutorial_completed = recent_whatched_time === duration ? true : false
             get_video_progress.video_tutorial_duration = duration
             const updated = await get_video_progress.save()
-            
+
             if (updated) {
                 res.status(200)
                     .json({ message: "video progress updated successfully", success: true, video_progress: updated })
@@ -113,7 +113,7 @@ const create_lesson_progress = async (req, res) => {
 
 const update_lesson_progress = async (req, res) => {
     try {
-        const { tutorial_completed, assignment_completed, student_id, course_id, lesson_id ,video_progress} = req.body
+        const { tutorial_completed, assignment_completed, student_id, course_id, lesson_id, video_progress } = req.body
         const get_lesson_progress = await lesson_progress_model.findOne({ lesson_id })
         if (get_lesson_progress) {
             tutorial_completed ? get_lesson_progress.video_tutorial_completed = tutorial_completed : null
